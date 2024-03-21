839a353b01d1e4da201f8936329c2ee63a0e314d
Nishil <nmvisawadia@gmail.com>
2024-03-21 15:30:11 +0530

    add api

--- api/index.ts
+++ api/index.ts
@@ -3,6 +3,18 @@ const app = express();
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
+app.get('/add/:num1/:num2', (req, res) => {
+    const num1 = parseFloat(req.params.num1);
+    const num2 = parseFloat(req.params.num2);
+
+    if (isNaN(num1) || isNaN(num2)) {
+        return res.status(400).send('Invalid input. Please enter valid numbers.');
+    }
+
+    const sum = num1 + num2;
+    res.json({ result: sum });
+});
+
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
